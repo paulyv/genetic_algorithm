3a4e317c539d5601162c2bab23376f0ed25178e4
Pauli Varelius <pauli.varelius@gmail.com>
2017-08-13 15:10:48 +0300

    initial commit

---
+++ public/javascripts/genetic_algorithm.js
@@ -0,0 +1,66 @@
+var initial_fitness = 0;
+var delta_fitness = 0;
+var percentage_done = 0;
+
+$(document).ready(function() {
+
+    $("#button").bind('click', function(){
+      var target = $('#string').val();
+      var source = Array(target.length + 1).join("x");
+      $('#result').text(' ');
+      $('.progress').css('visibility', 'visible');
+      start(source, target);
+    });
+});
+
+
+function start(source, target) {
+  var fitval = fitness(source, target);
+  initial_fitness = fitval;
+  var count = 0;
+
+  var interval = setInterval(function() {
+  count++;
+
+  var m = mutate(source);
+  var fitval_m = fitness(m, target);
+
+  if(fitval_m === 0) {
+    clearInterval(interval);
+    var secs = (count * 35) / 1000;
+    $('#result').text("Done in " + secs + " seconds.");
+  }
+
+  if(fitval_m < fitval) {
+     fitval = fitval_m;
+     source = m;
+    // $('#genetic').text(count + " " + fitval_m + " " + m);
+     $('#evo').text(count);
+     $('#fit').text(fitval_m);
+     $('#cur').text(m);
+
+     delta_fitness = (initial_fitness - fitval_m);
+     percentage_done = (delta_fitness / initial_fitness) * 100;
+
+     $('.progress-bar').css('width', percentage_done + '%').attr('aria-valuenow', percentage_done);
+     $('.progress-bar').text(percentage_done.toFixed(1) + '%');
+  }
+}, 35);
+}
+
+function fitness(source, target) {
+  var fitval = 0;
+  for (var i = 0; i < source.length; i++) {
+    fitval += Math.abs(source.charCodeAt(i) - target.charCodeAt(i));
+  }
+  return fitval;
+}
+
+function mutate(source) {
+  var charpos = Math.floor(Math.random() * (source.length));
+  var parts = source.split("");
+  var delta = Math.random() < 0.5 ? -1 : 1;
+  var new_char = String.fromCharCode(source.charCodeAt(charpos) + delta);
+  parts[charpos] = new_char;
+  return parts.join('');
+}
